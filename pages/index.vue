db9e5b50ec97e769cca92fe4f7c46a4fd9e04456
Luis Felipe Bertel <ellipe@gmail.com>
2020-09-21 21:03:53 -0500

    init

---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="10" md="10">
+      <v-form ref="form" lazy-validation>
+        <v-text-field
+          outlined
+          v-model="name"
+          :counter="10"
+          :rules="[v => !!v || 'Name is required']"
+          placeholder="Name"
+          required
+        ></v-text-field>
+
+        <v-text-field
+          outlined
+          v-model="email"
+          :rules="[v => !!v || 'Email is required']"
+          placeholder="E-mail"
+          required
+        ></v-text-field>
+
+        <v-select
+          outlined
+          v-model="gender"
+          :items="options"
+          :rules="[v => !!v || 'Item is required']"
+          placeholder="Gender"
+          required
+        ></v-select>
+        <v-btn
+          color="primary"
+          class="mr-4"
+          block
+        >
+          Send
+        </v-btn>
+      </v-form>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      name: '',
+      email: '',
+      gender: '',
+    }
+  },
+  computed: {
+    options(){
+      return [
+        {text: 'Male', value: 'm'},
+        {text: 'Female', value: 'f'},
+        {text: 'Rather not say', value: 'u'},
+
+      ];
+    }
+  }
+};
+</script>
